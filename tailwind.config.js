856b117375c1e55bb1951baa954bed8b59ace4ab
noway <dragonfury1928@gmail.com>
2024-01-07 17:40:08 -0500

    basic padding and fonts + styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				burtons: "burtons",
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 }

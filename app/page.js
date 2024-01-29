58b6fc32f86d51ce328eeca5dcaea8b21d3d14de
noway <dragonfury1928@gmail.com>
2024-01-29 09:54:23 -0500

    removed old card types and gradient description

--- app/page.js
+++ app/page.js
@@ -23,22 +23,34 @@ export default function Home() {
 			</div>
 			<main className={'px-10 md:px-40 lg:px-40'}>
 				<section className={'min-h-screen flex flex-col items-center justify-center'}>
-					<div className={'flex flex-col items-center justify-center py-20'}>
+					<div className={'flex flex-col items-center justify-center py-14'}>
 						<h1 className={'text-5xl md:text-6xl font-bold text-white text-center'}>Camilo Alvarez-Velez</h1>
 						<h2 className={'text-2xl md:text-3xl font-bold text-white text-center'}>Software Engineer</h2>
 					</div>
-					<div className={'max-w-5xl w-full p-6 md:p-8 bg-blue-400 shadow-md rounded-2xl flex items-center bg-opacity-10'}>
+					<div className={'max-w-5xl w-full p-6 md:p-8 bg-blue-400 shadow-md rounded-2xl flex items-center bg-gradient-to-bl from-cyan-600 to-purple-500'}>
 						<div className={'w-1/2 pr-6 '}>
 
 							<p className={'text-lg py-2 text-gray-200'}>
 								I&apos;m a developer based in Orlando, FL. I love to create and build things that live on the internet.
-							</p>
-							<p className={'text-lg py-2 text-gray-200'}>
 								I am currently a student at the University of Central Florida pursuing a degree in Computer Science.
+								I am currently seeking internships as a Software Engineer. I am also a huge bookworm and love to read.
+								You can find me on <a className={'text-blue-500'} href="https://app.thestorygraph.com/profile/nowayreads" target="_blank" rel="noopener noreferrer">The StoryGraph</a> where I track my reading.
+
 							</p>
 							<p className={'text-lg py-2 text-gray-200'}>
-								I am currently seeking internships as a Software Engineer.
+								Here are a few technologies I&apos;ve been working with recently:
 							</p>
+							<ul className={'text-lg py-2 text-gray-200 '}>
+								<li>JavaScript (ES6+)</li>
+								<li>React</li>
+								<li>Next.js</li>
+								<li>Node.js</li>
+								<li>GraphQL</li>
+								<li>Python</li>
+								<li>Java</li>
+								<li>C++</li>
+							</ul>
+
 						</div>
 
 						{/* Image Section */}
@@ -55,9 +67,9 @@ export default function Home() {
 						</div>
 					</div>
 				</section>
-				<section style={{backgroundColor: '#37456b'}} className={"px-4 rounded-lg"}>
-					<div className={'flex flex-col md:flex-row items-center'}>
-						<div className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-800 text-white">
+				<section style={{backgroundColor: '#37456b'}} className={"px-4 py-8 rounded-lg "}>
+					<div className={'flex flex-col justify-around md:flex-row items-center'}>
+						<div className="max-w-sm rounded overflow-hidden shadow-2xl bg-gray-800 text-white hover:scale-110 duration-300 ">
 							<img className="w-full" src="/card-top.jpg" alt="Sunset in the mountains"/>
 							<div className="px-6 py-4">
 								<div className="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -69,57 +81,37 @@ export default function Home() {
 								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
 								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
 								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+
 							</div>
 						</div>
-						<div className={"flex-row lg:flex gap-10"}>
-							<div className={"text-center shadow-lg p-10 rounded-xl my-10 flex-1"}>
-								<Image src={Headshot} width={100} height={100} alt={"Design Icon"}/>
-								<h3 className={"text-lg font-medium pt-8 pb-2 "}>Designs</h3>
-								<p className="text-gray-800 py-1">Elegant code for your needs</p>
-								<h4 className={"py-4 text-teal-600"}>What I use: </h4>
-								<ul className={"*:inline-block *:rounded-full *:border *:border-sky-100 *:bg-sky-50 *:px-3 *:py-1 text-sky-300 *:border-sky-500/15 *:bg-sky-500/10 *:space-x-2"}>
-									<li className="m-1">MySQL</li>
-									<li className="m-1">MongoDB</li>
-									<li className="m-1">Next.js</li>
-									<li className="m-1">Node.js</li>
-									<li className="m-1">Express.js</li>
-									<li className="m-1">React</li>
-									<li className="m-1">React Native</li>
-									<li className="m-1">TailwindCSS</li>
-									<li className="m-1">HTML</li>
-									<li className="m-1">CSS</li>
-								</ul>
+						<div className="max-w-sm rounded overflow-hidden shadow-2xl bg-gray-800 text-white hover:scale-110 duration-300 ">
+							<img className="w-full" src="/card-top.jpg" alt="Sunset in the mountains"/>
+							<div className="px-6 py-4">
+								<div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+								<p className="text-gray-700 text-base">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+								</p>
+							</div>
+							<div className="px-6 pt-4 pb-2">
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
 
 							</div>
-
-							<div className={"bg-sky-500/[.06] opacity-90 text-center shadow-lg p-10 rounded-xl my-10 flex-1"}>
-								<Image src={Headshot} width={100} height={100} alt={"Consulting Icon"}/>
-								<h3 className={"text-lg font-medium pt-8 pb-2"}>Designs</h3>
-								<p className="text-gray-200 py-1">Elegant code for your needs</p>
-								<h4 className={"py-4 text-teal-600"}>What I use: </h4>
-								<p className="text-gray-200 py-1">C, Java, JS</p>
-								<p className="text-gray-200 py-1">MySQL, MongoDB</p>
-								<p className="text-gray-200 py-1">Next.js, Node.js, Express.js</p>
-								<p className="text-gray-200 py-1">React, React Native, TailwindCSS, HTML, CSS</p>
+						</div>
+						<div className="max-w-sm rounded overflow-hidden shadow-2xl bg-gray-800 text-white hover:scale-110 duration-300 ">
+							<img className="w-full" src="/card-top.jpg" alt="Sunset in the mountains"/>
+							<div className="px-6 py-4">
+								<div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+								<p className="text-gray-700 text-base">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+								</p>
 							</div>
-							<div className={"bg-sky-500/[.06] opacity-90 text-center shadow-lg p-10 rounded-xl my-10 flex-1"}>
-								<Image src={Headshot} width={100} height={100} alt={"Consulting Icon"}/>
-								<h3 className={"text-lg font-medium pt-8 pb-2"}>Designs</h3>
-								<p className="text-gray-200 py-1">Elegant code for your needs</p>
-								<h4 className={"py-4 text-teal-600"}>What I use: </h4>
-								<p className="text-gray-200 py-1">C, Java, JS</p>
-								<p className="text-gray-200 py-1">MySQL, MongoDB</p>
-								<p className="text-gray-200 py-1">Next.js, Node.js, Express.js</p>
-								<p className="text-gray-200 py-1">React, React Native, TailwindCSS, HTML, CSS</p>
+							<div className="px-6 pt-4 pb-2">
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+								<span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+
 							</div>
 						</div>
 					</div>
